cc6429d33f54fbaba09daabc2462f2875ecf04cf
Gurgen Yeganyan <gygnyn@gmail.com>
2022-11-30 00:38:22 +0400

    dockerized

--- project/script.py
+++ project/script.py
@@ -1,4 +1,7 @@
-from data.db import create_database, transcription_query, translation_query
+import sys
+
+from data.db import transcription_query, translation_query
+from gc_ratio.plotting import represent_gc_ratio
 
 
 def convert_dna_to_rna(dna_strand: str) -> str:
@@ -30,9 +33,32 @@ def convert_rna_to_protein(m_rna: str) -> str:
         raw_protein.append(translation_query(str_triplet))
         current_triplet.clear()
         current_index += 1
-
     return ''.join(raw_protein)
 
 
+def cli_tool():
+    def execute_command(command, input_str, file=False):
+        if command == 'transcribe':
+            print(convert_dna_to_rna(input_str))
+        if command == 'translate':
+            print(convert_rna_to_protein(input_str))
+        if command == 'plot':
+            plot_window = sys.argv[4] if file else sys.argv[3]
+            represent_gc_ratio((input_str), int(plot_window))
+
+    if sys.argv[1] == 'file':
+        current_command = sys.argv[2]
+        file_name = sys.argv[3]
+        with open(f'data/inputs/{file_name}') as file:
+            input_data = file.read()
+
+        execute_command(current_command, input_data, file=True)
+    else:
+        current_command = sys.argv[1]
+        input_data = sys.argv[2]
+
+        execute_command(current_command, input_data)
+
+
 if __name__ == '__main__':
-    create_database()
+    cli_tool()
